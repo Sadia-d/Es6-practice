dee8d8e191c8cd1e787afdc404abab73a34f4504
Sadia-d <subai>
2025-10-24 13:52:53 +0600

    practice.js

---
+++ practice.js
@@ -0,0 +1,14 @@
+/// problem 1 : write an arrow function that will take 3 perameters , will multiply the perameters and return result.
+
+const multiply = (num1 ,num2 , num3)=>{
+     return num1 * num2 * num3
+}
+// console.log(multiply(2,3,4));
+
+/// problem 1 : write the the sentence in three lines and print the result . I am web developer . I love code . I love to eat biriany.
+
+const sentence = `I am web developer. 
+I love code. 
+I love to eat biriany.`
+
+// console.log(sentence);
